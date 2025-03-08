37aaad729a6feecf1a7bae381eead51d2b7f590f
MohamedYehia20m <medoyehia2001@gmail.com>
2025-03-08 23:50:11 +0200

    fix(security) : update CORS policy

--- movie/src/main/java/com/fawry/movie/config/SecurityConfig.java
+++ movie/src/main/java/com/fawry/movie/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
